b6382f208d6edd08b46a5fed35a49faf83f0e8d1
Drewmsu <fran.mp07@gmail.com>
2020-07-20 02:15:44 -0500

    ios splash screen fix

--- App.js
+++ App.js
@@ -1,17 +1,93 @@
 import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
+import AsyncStorage from '@react-native-community/async-storage';
+import { Asset } from 'react-native-unimodules';
 import { Provider as PaperProvider } from 'react-native-paper';
+import * as SplashScreen from 'expo-splash-screen';
 import RootNavigator from './src/navigation/RootNavigator';
 import { appTheme } from './src/theme/appTheme';
+import constants from './src/utils/constants';
+import { urls } from './src/services/api/afari';
 
 const AppContainer = createAppContainer(RootNavigator);
 
 export default class App extends Component {
+  state = {
+    appIsReady: false
+  };
+
+  async componentDidMount() {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
+    }
+    this.prepareResources();
+  }
+
+  prepareResources = async () => {
+    await performAPICalls();
+    await downloadAssets();
+
+    this.setState({ appIsReady: true }, async () => {
+      await SplashScreen.hideAsync();
+    });
+  };
+
   render() {
+    if (!this.state.appIsReady) {
+      return null;
+    }
+
     return (
       <PaperProvider theme={appTheme}>
         <AppContainer />
       </PaperProvider>
     );
   }
+}
+
+async function performAPICalls() {
+  await _loginHandler();
+}
+
+async function downloadAssets() {
+  return Promise.all([Asset.loadAsync([require('./assets/images/logo.png')])]);
+}
+
+async function _loginHandler() {
+  const userCredentials = {
+    username: await AsyncStorage.getItem(constants.user.username),
+    password: await AsyncStorage.getItem(constants.user.password)
+  };
+  await fetch(urls.login, {
+    method: 'POST',
+    body: JSON.stringify({
+      usuario: userCredentials.username,
+      password: userCredentials.password
+    }),
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' }
+  })
+    .then((response) => response.json())
+    .then((parsedResponse) => {
+      if (parsedResponse.error) {
+        console.log(parsedResponse.mensaje);
+      } else {
+        AsyncStorage.setItem(constants.user.userId, String(parsedResponse.usuarioId));
+        AsyncStorage.setItem(constants.user.role, String(parsedResponse.rol));
+        AsyncStorage.setItem(constants.user.email, String(parsedResponse.correo));
+        AsyncStorage.setItem(constants.user.name, String(parsedResponse.nombre));
+        AsyncStorage.setItem(constants.user.lastPayment, String(parsedResponse.ultimaPagada));
+        AsyncStorage.setItem(constants.building.id, String(parsedResponse.edificioId));
+        AsyncStorage.setItem(constants.building.name, String(parsedResponse.nombreEdificio));
+        AsyncStorage.setItem(constants.building.apartmentId, String(parsedResponse.departamentoId));
+        AsyncStorage.setItem(
+          constants.building.apartmentName,
+          String(parsedResponse.nombreDepartamento)
+        );
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
--- src/screens/shared/AuthLoadingScreen.js
+++ src/screens/shared/AuthLoadingScreen.js
@@ -1,74 +1,14 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, StatusBar } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
 import { WaveIndicator } from 'react-native-indicators';
 import { colors } from '../../theme/appTheme';
 import constants from '../../utils/constants';
-import { urls } from '../../services/api/afari';
 
 class AuthLoadingScreen extends Component {
-  state = {
-    resourcesReady: false
-  };
-
-  async _loadResourcesAsync() {
-    return Promise.all([Asset.loadAsync([require('../../../assets/images/logo.png')])]);
+  async componentDidMount() {
+    await this._bootstrapAsync();
   }
-
-  _loginHandler = async () => {
-    const userCredentials = {
-      username: await AsyncStorage.getItem(constants.user.username),
-      password: await AsyncStorage.getItem(constants.user.password)
-    };
-    await fetch(urls.login, {
-      method: 'POST',
-      body: JSON.stringify({
-        usuario: userCredentials.username,
-        password: userCredentials.password
-      }),
-      headers: { Accept: 'application/json', 'Content-Type': 'application/json' }
-    })
-      .then((response) => response.json())
-      .then((parsedResponse) => {
-        if (parsedResponse.error) {
-          console.log(parsedResponse.mensaje);
-        } else {
-          AsyncStorage.setItem(constants.user.userId, String(parsedResponse.usuarioId));
-          AsyncStorage.setItem(constants.user.role, String(parsedResponse.rol));
-          AsyncStorage.setItem(constants.user.email, String(parsedResponse.correo));
-          AsyncStorage.setItem(constants.user.name, String(parsedResponse.nombre));
-          AsyncStorage.setItem(constants.user.lastPayment, String(parsedResponse.ultimaPagada));
-          AsyncStorage.setItem(constants.building.id, String(parsedResponse.edificioId));
-          AsyncStorage.setItem(constants.building.name, String(parsedResponse.nombreEdificio));
-          AsyncStorage.setItem(
-            constants.building.apartmentId,
-            String(parsedResponse.departamentoId)
-          );
-          AsyncStorage.setItem(
-            constants.building.apartmentName,
-            String(parsedResponse.nombreDepartamento)
-          );
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  _startAsyncTasks = async () => {
-    await this._loadResourcesAsync();
-    await this._loginHandler();
-  };
-
-  _handleLoadingError = (error) => {
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ resourcesReady: true });
-  };
 
   _bootstrapAsync = async () => {
     const userId = parseInt(await AsyncStorage.getItem(constants.user.userId));
@@ -84,29 +24,13 @@ class AuthLoadingScreen extends Component {
     }
   };
 
-  componentDidUpdate() {
-    if (this.state.resourcesReady) {
-      this._bootstrapAsync();
-    }
-  }
-
   render() {
-    if (!this.state.resourcesReady) {
-      return (
-        <AppLoading
-          startAsync={this._startAsyncTasks}
-          onFinish={this._handleFinishLoading}
-          onError={this._handleLoadingError}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          <StatusBar barStyle="light-content" />
-          <WaveIndicator color={colors.afariGreen} size={100} />
-        </View>
-      );
-    }
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <WaveIndicator color={colors.afariGreen} size={100} />
+      </View>
+    );
   }
 }
 
